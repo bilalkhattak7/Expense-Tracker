66658a1808f163a2474496b4b97cb4a91856b244
Bilalkhattak7 <bilalkhan381810@gmail.com>
2025-09-10 13:43:12 +0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:expense_tracker/Screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Expense Tracker",
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
